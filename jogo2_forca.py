72ae959a7b50aa4ad4b61058c5f6ce54b844bac2
liviaalvzs <livs.santana01@gmail.com>
2021-07-12 16:56:39 -0300

    incrementando exibição da quantidade de acertos restantes

--- jogo2_forca.py
+++ jogo2_forca.py
@@ -3,32 +3,39 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "livro"
-    letras_acertadas = ["_", "_", "_", "_", "_"]
+    palavra_secreta = "maça".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
-    tentativas = 0
-    index = 0
+    erros = 0
 
     print(letras_acertadas)
 
     while(not enforcou and not acertou):
-        chute = input("Qual letra? ")
-        chute = chute.strip()
-        
-        if(chute in palavra_secreta):
-            for letra in palavra_secreta:
-                if(chute.upper() == letra.upper()):
-                    letras_acertadas[index] = letra
-                index = index + 1
-        else:
-            tentativas += 1
-        
-        enforcou = tentativas == 6
 
+        chute = input("Qual letra? ")
+        chute = chute.strip().upper()
+
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+            print("Ops, você errou! Faltam {} tentativas.".format(6-erros))
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
 
+
+    if(acertou):
+        print("Você ganhou!!")
+    else:
+        print("Você perdeu!!")
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
